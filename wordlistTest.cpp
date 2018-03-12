0bac253644d7f1410820b9e8c8477c5f713dc843
thegajan <thegajannagaraj@gmail.com>
2018-03-11 18:01:11 -0700

    finished wordlist test

--- wordlistTest.cpp
+++ wordlistTest.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <cassert>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
 int main() {
@@ -12,14 +14,38 @@ int main() {
 		cout << "Unable to load word list" << endl;
 		return 0;
 	}
-	assert(wl.contains("onomatopoeia"));
-	assert(wl.contains("'em"));
+		assert(wl.contains("onomatopoeia"));
+		assert(wl.contains("AaH"));
+		assert(wl.contains("'em"));
+		assert(!wl.contains("gajan"));
+	
+		string cipher = "xyqbbq";
+		string decodedSoFar = "?r????";
 
-	string cipher = "xyqbbq";
-	string decodedSoFar = "?r????";
-	vector<string> v = wl.findCandidates(cipher, decodedSoFar);
-	assert(!v.empty());
-	for (size_t k = 0; k < v.size(); k++)
-		assert(v[k] == "grotto" || v[k] == "troppo");
-	cout << "Passed all Test Cases" << endl;
+		vector<string> v = wl.findCandidates(cipher, decodedSoFar);
+		assert(!v.empty());
+		for (size_t k = 0; k < v.size(); k++)
+			assert(v[k] == "grotto" || v[k] == "troppo");
+
+		decodedSoFar = "??????";
+		v = wl.findCandidates(cipher, decodedSoFar);
+		assert(!v.empty());
+		string arr[] = { "apollo", "blotto", "career", "chilli", "djinni", "fusees", "grotto", "guerre", "indeed", "piazza", "pierre", "quagga", "steppe", "troppo" };
+		for (size_t k = 0; k < v.size(); k++) {
+			assert(find(begin(arr), end(arr), v[k]));
+		}
+
+		decodedSoFar = "??o??o";
+		v = wl.findCandidates(cipher, decodedSoFar);
+		assert(!v.empty());
+		string arr2[] = { "apollo", "blotto","grotto", "troppo" };
+		for (size_t k = 0; k < v.size(); k++) {
+			assert(find(begin(arr2), end(arr2), v[k]));
+		}
+
+		decodedSoFar = "???mm?";
+		v = wl.findCandidates(cipher, decodedSoFar);
+		assert(v.empty());
+
+		cout << "Passed all Test Cases" << endl;
 }
