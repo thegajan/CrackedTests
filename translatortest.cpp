63617020b371bee6f6afe702c23faf8dc7e2350c
thegajan <thegajannagaraj@gmail.com>
2018-03-12 00:13:29 -0700

    translator test

---
+++ translatortest.cpp
@@ -0,0 +1,37 @@
+#include "provided.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+int main() {
+	Translator t; 
+	string secret = "Hdqlx!";
+	string translated = t.getTranslation(secret);
+	assert(translated == "?????!");
+
+	t.pushMapping("DHL", "ERD");
+	translated = t.getTranslation(secret);
+	assert(translated == "Re?d?!");
+
+	t.pushMapping("QX", "AY");
+	translated = t.getTranslation(secret);
+	assert(translated == "Ready!");
+
+	t.popMapping();
+	translated = t.getTranslation(secret);
+	assert(translated == "Re?d?!");
+
+	t.popMapping();
+	translated = t.getTranslation(secret);
+	assert(translated == "?????!");
+
+	t.pushMapping("DHL", "ERD");
+	assert(!t.pushMapping("QX", "RY"));
+	translated = t.getTranslation("HDX");
+	assert(translated == "RE?");
+	assert(!t.pushMapping("H", "S"));
+
+	cout << "Passed all tests." << endl;
+}
